2076c20c24e3cd6943498495167c3013684cc00e
Dean Masley <grins@Deans-MacBook-Pro.local>
2025-02-28 17:10:06 +0100

    remove unused

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -7,19 +7,6 @@
 	import { DecalMaterial } from '$lib/shaders/decalMaterial';
 	import HudScene from './HUDScene.svelte';
 
-	type Event = THREE.Intersection & {
-		intersections: THREE.Intersection[]; // The first intersection of each intersected object
-		object: THREE.Object3D; // The object that was actually hit
-		eventObject: THREE.Object3D; // The object that registered the event
-		camera: THREE.Camera; // The camera used for raycasting
-		delta: THREE.Vector2; //  Distance between mouse down and mouse up event in pixels
-		nativeEvent: MouseEvent | PointerEvent | WheelEvent; // The native browser event
-		pointer: THREE.Vector2; // The pointer position in normalized device coordinates
-		ray: THREE.Ray; // The ray used for raycasting
-		stopPropagation: () => void; // Function to stop propagation of the event
-		stopped: boolean; // Whether the event propagation has been stopped
-	};
-
 	const { camera } = useThrelte();
 
 	let intersectionPoint: THREE.Vector3 | null = $state(null);
